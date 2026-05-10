f4c80052673a059b34e0a0448a193e7a94f1ba08
rasya andi f h <rasyaf010@gmail.com>
2026-05-10 18:47:10 +0700

    NEW UPDATE

--- e_kat.php
+++ e_kat.php
@@ -24,7 +24,7 @@ if (isset($_POST['update'])) {
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Kategoori Produk - InvAdmin</title>
+  <title>Kategoori Produk - inventory rasya</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -57,7 +57,7 @@ if (isset($_POST['update'])) {
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.php" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">InvAdmin</span>
+        <span class="d-none d-lg-block">inventory rasya</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
